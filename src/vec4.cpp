7bd7485cd2f3a11b0d5aa94e0e623abd50d210c8
hammy <mileslacey@gmail.com>
2018-09-17 01:13:46 -0700

    Fixing Clang 5.0.0 build. Class template instantiation has no effect after explicit specialization.
    Fixing OS X build by removing POSIX-incompatible sed regex.

--- src/vec4.cpp
+++ src/vec4.cpp
@@ -9,7 +9,11 @@ namespace math {
 /*
  * 4D Vector Specializations
  */
-LS_DEFINE_UNION_TYPE(vec4_t, float);
+#if !defined(LS_ARCH_X86) && !defined(LS_ARCH_ARM)
+    LS_DEFINE_UNION_TYPE(vec4_t, float);
+
+#endif
+
 LS_DEFINE_UNION_TYPE(vec4_t, double);
 LS_DEFINE_UNION_TYPE(vec4_t, int);
 LS_DEFINE_UNION_TYPE(vec4_t, unsigned);
