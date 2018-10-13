c83bbe5f421d57b8bbdd08d1d4bb005725e1f470
hammy <mileslacey@gmail.com>
2018-10-13 02:52:57 -0700

    Fixed matrix-vector multiplication on ARM NEON.

--- include/lightsky/math/arm/mat4f_impl.h
+++ include/lightsky/math/arm/mat4f_impl.h
@@ -9,13 +9,22 @@ namespace math {
 -------------------------------------*/
 inline vec4_t<float> vec4_t<float>::operator*(const mat4_t<float>& m) const
 {
-    float32x4_t ret;
-    ret = vmulq_f32(     m[0].simd, simd);
-    ret = vmlaq_f32(ret, m[1].simd, simd);
-    ret = vmlaq_f32(ret, m[2].simd, simd);
-    ret = vmlaq_f32(ret, m[3].simd, simd);
-    
-    return vec4_t<float>{ret};
+    const float32x4_t aq = vmulq_f32(m[0].simd, simd);
+    const float32x4_t bq = vmulq_f32(m[1].simd, simd);
+    const float32x4_t cq = vmulq_f32(m[2].simd, simd);
+    const float32x4_t dq = vmulq_f32(m[3].simd, simd);
+
+    const float32x2_t ad = vadd_f32(vget_high_f32(aq), vget_low_f32(aq));
+    const float32x2_t bd = vadd_f32(vget_high_f32(bq), vget_low_f32(bq));
+    const float32x2_t cd = vadd_f32(vget_high_f32(cq), vget_low_f32(cq));
+    const float32x2_t dd = vadd_f32(vget_high_f32(dq), vget_low_f32(dq));
+
+    return vec4_t<float>{
+        vget_lane_f32(vpadd_f32(ad, ad), 0),
+        vget_lane_f32(vpadd_f32(bd, bd), 0),
+        vget_lane_f32(vpadd_f32(cd, cd), 0),
+        vget_lane_f32(vpadd_f32(dd, dd), 0)
+    };
 }
 
 
