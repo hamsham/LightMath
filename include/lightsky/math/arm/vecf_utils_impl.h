40ef0cf0fc23970cee02ce3a1bac318a73c8cdbf
hammy <mileslacey@gmail.com>
2020-08-06 16:04:16 -0700

    Reduced instruction latency of a sign-mask gather for NEON.

--- include/lightsky/math/arm/vecf_utils_impl.h
+++ include/lightsky/math/arm/vecf_utils_impl.h
@@ -221,18 +221,22 @@ inline LS_INLINE vec4_t<float> rcp(const vec4_t<float>& v) noexcept
 -------------------------------------*/
 inline LS_INLINE int sign_mask(const vec4_t<float>& x) noexcept
 {
-    const int32x4_t shiftLiterals{-31, -30, -29, -28};
-    const int32x4_t sign  = vdupq_n_u32(0x80000000);
-    const int32x4_t cmp   = vandq_u32(sign, vreinterpretq_u32_f32(x.simd));
-    const int32x4_t masks = vreinterpretq_s32_u32(vqshlq_u32(cmp, shiftLiterals));
-
     #if defined(LS_ARCH_AARCH64)
+        const int32x4_t andLiterals{0x01, 0x02, 0x04, 0x08};
+        const int32x4_t cmp   = vreinterpretq_s32_u32(vcltzq_f32(x.simd));
+        const int32x4_t masks = vandq_s32(andLiterals, cmp);
+
         // Adding powers-of-two only sets the corresponding bits.
         // This is a low-throughput instruction and only works now because it
         // replaces the 6 instructions in the Arm32 version.
         return vaddvq_s32(masks);
 
     #else
+        const int32x4_t shiftLiterals{-31, -30, -29, -28};
+        const int32x4_t sign  = vdupq_n_u32(0x80000000);
+        const int32x4_t cmp   = vandq_u32(sign, vreinterpretq_u32_f32(x.simd));
+        const int32x4_t masks = vreinterpretq_s32_u32(vqshlq_u32(cmp, shiftLiterals));
+
         const int32x2_t lo    = vget_low_s32(masks);
         const int32x2_t hi    = vget_high_s32(masks);
         const int32x2_t swp   = vorr_s32(hi, lo);
