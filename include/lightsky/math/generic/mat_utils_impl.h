280c09e1e86feaa9814d393a96956cd69223f5bf
hammy <mileslacey@gmail.com>
2020-08-19 14:32:27 -0700

    Fixed broken perspective matrix.

--- include/lightsky/math/generic/mat_utils_impl.h
+++ include/lightsky/math/generic/mat_utils_impl.h
@@ -460,17 +460,19 @@ math::mat4_t<num_t> math::translate(const mat4_t<num_t>& m, const vec3_t<num_t>&
 -------------------------------------*/
 template <typename num_t> inline LS_INLINE
 math::mat4_t<num_t> math::perspective(num_t fov, num_t aspect, num_t zNear, num_t zFar) noexcept {
-    const num_t top = LS_TAN(fov / num_t{2}) * zNear;
-    const num_t bottom = -top;
-    const num_t xMin = bottom * aspect;
-    const num_t xMax = top * aspect;
-    const num_t zDelta = zFar - zNear;
+    const num_t right  = zNear * LS_TAN(fov * num_t{2});
+    const num_t left   = -right;
+    const num_t bottom = left / aspect;
+    const num_t top    = right / aspect;
+    const num_t x      = right - left;
+    const num_t y      = top - bottom;
+    const num_t z      = zFar - zNear;
 
     return mat4_t<num_t>{
-        (num_t{2}*zNear) / (xMax - xMin), num_t{0},                          num_t{0},                          num_t{0},
-        num_t{0},                         (num_t{2}*zNear) / (top - bottom), num_t{0},                          num_t{0},
-        num_t{0},                         num_t{0},                          -(zFar + zNear) / zDelta,          num_t{-1},
-        num_t{0},                         num_t{0},                          (num_t{-2}*zFar * zNear) / zDelta, num_t{0}
+        (num_t{2}*zNear)/x, num_t{0},           num_t{0},                 num_t{0},
+        num_t{0},           (num_t{2}*zNear)/y, num_t{0},                 num_t{0},
+        (right+left)/x,     (top+bottom)/y,     -(zFar+zNear)/z,          num_t{-1},
+        num_t{0},           num_t{0},           -(num_t{2}*zFar*zNear)/z, num_t{0}
     };
 }
 
@@ -532,10 +534,10 @@ math::mat4_t<num_t> math::frustum(num_t left, num_t right, num_t top, num_t bott
     const num_t d = zFar - zNear;
 
     return mat4_t<num_t>{
-        (num_t{2}*zNear) / w,  num_t {0},            num_t{0},                    num_t{0},
-        num_t{0},             (num_t{2}*zNear) / h,  num_t{0},                    num_t{0},
-        (right + left) / w,   (top + bottom) / h,    -(zFar + zNear) / d,          num_t{-1},
-        num_t{0},             num_t{0},              (num_t{-2}*zFar * zNear) / d, num_t{0}
+        (num_t{2}*zNear)/w, num_t {0},         num_t{0},                num_t{0},
+        num_t{0},          (num_t{2}*zNear)/h, num_t{0},                num_t{0},
+        (right+left)/w,    (top+bottom)/h,    -(zFar+zNear)/d,          num_t{-1},
+        num_t{0},          num_t{0},          -(num_t{2}*zFar*zNear)/d, num_t{0}
     };
 }
 
