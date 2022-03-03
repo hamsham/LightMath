650ed3e7b4868ce9a203723ad8857a2232bcd85e
hammy <mileslacey@gmail.com>
2022-03-02 23:29:43 -0800

    Fixed broken namespacing

--- include/lightsky/math/generic/fixed_impl.h
+++ include/lightsky/math/generic/fixed_impl.h
@@ -747,7 +747,7 @@ template<typename fixed_base_t, unsigned num_frac_digits>
 inline LS_INLINE const math::fixed_t<fixed_base_t, num_frac_digits> math::sin(const math::fixed_t<fixed_base_t, num_frac_digits>& x) noexcept
 {
     typedef ls::math::fixed_t<fixed_base_t, num_frac_digits> FixedType;
-    return sin_step<FixedType>(ls::math::abs<FixedType>(ls::math::fract<FixedType>((x - FixedType{LS_PI_OVER_2}) / FixedType{LS_TWO_PI}) * FixedType{2.0} - FixedType{1.0}));
+    return ls::math::impl::sin_step<FixedType>(ls::math::abs<FixedType>(ls::math::fract<FixedType>((x - FixedType{LS_PI_OVER_2}) / FixedType{LS_TWO_PI}) * FixedType{2.0} - FixedType{1.0}));
 }
 
 
@@ -759,7 +759,7 @@ template<typename fixed_base_t, unsigned num_frac_digits>
 inline LS_INLINE const math::fixed_t<fixed_base_t, num_frac_digits> math::cos(const math::fixed_t<fixed_base_t, num_frac_digits>& x) noexcept
 {
     typedef ls::math::fixed_t<fixed_base_t, num_frac_digits> FixedType;
-    return sin_step<FixedType>(ls::math::abs<FixedType>(ls::math::fract<FixedType>(x / FixedType{LS_TWO_PI}) * FixedType{2.0} - FixedType{1.0}));
+    return ls::math::impl::sin_step<FixedType>(ls::math::abs<FixedType>(ls::math::fract<FixedType>(x / FixedType{LS_TWO_PI}) * FixedType{2.0} - FixedType{1.0}));
 }
 
 
