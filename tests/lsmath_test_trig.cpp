f8685b5c9924e79636ab760111ad47a40f51b917
hammy <mileslacey@gmail.com>
2025-06-11 21:57:25 -0700

    Testing more accurate sin/cos functions.

--- tests/lsmath_test_trig.cpp
+++ tests/lsmath_test_trig.cpp
@@ -5,6 +5,67 @@
 #include "lightsky/math/scalar_utils.h"
 
 
+// Un-vectorized from v_sincos(), in:
+// https://github.com/GaijinEntertainment/vecmath/blob/master/dag_vecMath_trig.h
+// BSD license as of Jun 11, 2025
+void math_sincos(float theta, float& s, float& c) noexcept
+{
+    float xl;
+    xl = theta * (float)(2.0 * LS_PI_INVERSE);
+    xl = xl + (theta >= 0.f ? 0.5f : -0.5f);
+
+    const int q = (int)xl;
+
+    const int offsetSin = q & 3;
+    const int offsetCos = 1 + offsetSin;
+
+    const float qf = (float)q;
+    const float p1 = theta - (qf * 1.57079625129f);
+    xl = p1 - (qf * 7.54978995489e-8f);
+
+    const float xl2 = xl * xl;
+    const float xl3 = xl2 * xl;
+
+    const float ct1 = (-0.0013602249f * xl2) + 0.0416566950f;
+    const float st1 = (-0.0001950727f * xl2) + 0.0083320758f;
+
+    const float ct2 = (ct1 * xl2) + -0.4999990225f;
+    const float st2 = (st1 * xl2) + -0.1666665247f;
+
+    const float cx = (ct2 * xl2) + 1.f;
+    const float sx = (st2 * xl3) + xl;
+
+    int sinMask = !(offsetSin & 1);
+    int cosMask = !(offsetCos & 1);
+    s = sinMask ? sx : cx;
+    c = cosMask ? sx : cx;
+
+    sinMask = !(offsetSin & 2);
+    cosMask = !(offsetCos & 2);
+
+    s = sinMask ? s : -s;
+    c = cosMask ? c : -c;
+}
+
+
+
+float math_sin(float theta) noexcept
+{
+    float s, c;
+    math_sincos(theta, s, c);
+    return s;
+}
+
+
+
+float math_cos(float theta) noexcept
+{
+    float s, c;
+    math_sincos(theta, s, c);
+    return c;
+}
+
+
 
 void test_trig_values(float x, float (*pStdFunc)(float), float (*pLsFunc)(float))
 {
@@ -14,7 +75,7 @@ void test_trig_values(float x, float (*pStdFunc)(float), float (*pLsFunc)(float)
     const float errAbs = ls::math::abs(baseImpl - testImpl);
     const float errRel = 100.f * (errAbs / ls::math::abs(baseImpl));
 
-    printf("%1.1f, %2.9f, %2.9f, %2.9f, %2.9f", x, baseImpl, testImpl, errAbs, errRel);
+    printf("%1.1f | %2.9f | %2.9f | %2.9f | %2.9f", x, baseImpl, testImpl, errAbs, errRel);
 }
 
 
